370ab798242fe2a6564d062df8e452e8571c181f
camilo <cyateya35439@universidadean.edu.co>
2018-06-14 08:30:58 -0500

    Pagina Utilizada https://xkcd.com/1519/ con las imagenes descargadas

---
+++ web_scraping.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlretrieve
+
+
+
+def run():
+    for i in range(1,40):
+        response = requests.get('https://xkcd.com/{}'.format(i))
+        soup = BeautifulSoup(response.content, 'html.parser')
+        image_container = soup.find(id='comic')
+
+        image_url = image_container.find('img')['src']
+        image_name = image_url.split('/')[-1]
+        print('Descangando la imagen {}'.format(image_name))
+        #Solucion para
+        urlretrieve('https:{}'.format(image_url),image_name)
+
+
+if __name__ == '__main__':
+    run()
